e322a90e5dce130b1c593ead7128a9e0e2bf5483
XHY <504224624@qq.com>
2024-04-02 18:20:12 +0800

    配置接口地址

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,23 @@ import axios from './api' // 倒入 api
 
 export const getRoleData = (params) => {
   return axios({
-    url: '/home/category/head',
+    url: '/role/getPages',
+    method: 'GET',
+    params,
+  })
+}
+
+export const addRole = (params) => {
+  return axios({
+    url: '/role/add',
+    method: 'GET',
+    params,
+  })
+}
+
+export const deleteRole = (params) => {
+  return axios({
+    url: '/role/delById',
     method: 'GET',
     params,
   })
